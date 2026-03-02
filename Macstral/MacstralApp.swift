3aa77e0d450d5c8dd4511be973ef72392387294b
Yumi Izumi <mail@yumi.to>
2026-03-02 17:40:59 +0900

    Initial Commit

---
+++ Macstral/MacstralApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacstralApp.swift
+//  Macstral
+//
+//  Created by Yumi Izumi on 2026/03/02.
+//
+
+import SwiftUI
+
+@main
+struct MacstralApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
